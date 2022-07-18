904f15db69e5f44aefe8d9ae893e95b1337e3dcd
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-18 22:47:27 +0530

    [Sushree | Added Start Position for Snake and Ladder]

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int START_POSITION= 0;
+
+            Console.ReadLine();
+        }
+    }
+}
