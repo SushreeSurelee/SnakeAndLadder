4de038fa375f2065be933f54779e4855bea721bc
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-22 00:38:42 +0530

    [Sushree | added functionality to check winner between 2 players]

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,72 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
     class Program
     {
-        static int PlayOptions(int diceRoll, int dicePostion, int option)
+        static int PlayOptions(int diceRoll, int dicePostion, int option , int endPosition)
         {
             switch (option)
             {
                 case 0:
-                    dicePostion = dicePostion - diceRoll;
+                    dicePostion -= diceRoll;
                     break;
-
                 case 1:
-                    dicePostion = dicePostion + diceRoll;
+                    dicePostion += diceRoll;
                     break;
-
                 default:
                     break;
-
             }
-            return dicePostion;
+            return CheckEndPosition(dicePostion,endPosition,diceRoll);
         }
 
+        static int RandomValueGenerator( int start, int end)
+        {
+            Random random = new Random();
+            int rand = random.Next(start, end);
+            return rand;
+        }
+
+        static int CheckEndPosition(int playerPositionReached, int END_POSITION, int playerDiceRoll)
+        {
+            if (playerPositionReached > END_POSITION)
+            {
+                playerPositionReached -= playerDiceRoll;
+            }
+            return playerPositionReached;
+        }
         static void Main(string[] args)
         {
-
-            int START_POSITION= 0;
+            int START_POSITION = 0;
             int END_POSITION = 100;
-
-            Random random = new Random();
+            Console.WriteLine("Welcome to Snake and Ladder Game");
 
             string[] chooseOption = { "Snake", "Ladder", "Noplay" };
 
-            int positionReched = START_POSITION;
-            int count = 0;
-            while (positionReched != END_POSITION)
+            int playerOnePositionReached = START_POSITION;
+            int playerTwoPositionReached = START_POSITION;
+
+            while (true)
             {
-                if (positionReched < START_POSITION)
+                if (playerOnePositionReached < START_POSITION)
                 {
-                    positionReched = START_POSITION; 
+                    playerOnePositionReached = START_POSITION;
+                }
+                else if (playerTwoPositionReached < START_POSITION)
+                {
+                    playerTwoPositionReached = START_POSITION;
                 }
 
-                int diceRoll = random.Next(1, 7);
-                Console.WriteLine("Your dice value is :" + diceRoll);
+                int player1DiceRoll = RandomValueGenerator(1, 7);
+                Console.WriteLine("Player 1 dice value is :" + player1DiceRoll);
 
-                int option = random.Next(0, 3);
-                Console.WriteLine("You got:" + chooseOption[option]);
+                int player1_Option = RandomValueGenerator(0, 3);
+                Console.WriteLine("Player 1 got:" + chooseOption[player1_Option] + "\n");
 
-                positionReched = PlayOptions(diceRoll, positionReched, option);
+                int player2DiceRoll = RandomValueGenerator(1, 7);
+                Console.WriteLine("Player 2 dice value is :" + player2DiceRoll);
 
-                if(positionReched> END_POSITION)
+                int player2_Option = RandomValueGenerator(0, 3);
+                Console.WriteLine("Player 2 got:" + chooseOption[player2_Option] + "\n");
+
+                playerOnePositionReached = PlayOptions(player1DiceRoll, playerOnePositionReached, player1_Option , END_POSITION);
+                Console.WriteLine("Position reached by player 1 after dice roll: " + playerOnePositionReached);
+
+                playerTwoPositionReached = PlayOptions(player2DiceRoll, playerTwoPositionReached, player2_Option, END_POSITION);
+                Console.WriteLine("Position reached by player 2 after dice roll: " + playerTwoPositionReached + "\n");
+
+                if (playerOnePositionReached == END_POSITION) 
                 {
-                    positionReched = positionReched - diceRoll;
-                   
+                    Console.WriteLine("Player 1 have reached to :" + playerOnePositionReached + "\n ***PLAYER 1 WON THE GAME***");
+                    break;
                 }
-                Console.WriteLine("Dice Position after the dice roll is:" + positionReched);
-
-                count++;
+                else if (playerTwoPositionReached == END_POSITION)
+                {
+                    Console.WriteLine("Player 2 have reached to :" + playerTwoPositionReached + "\n ***PLAYER 2 WON THE GAME***");
+                    break;
+                }
+                
             }
-            
-            Console.WriteLine("you have reached to :" + positionReched +"\n ***You Won***");
-            Console.WriteLine("You have taken " + count + " turns to win the game.");
-
             Console.ReadLine();
         }
     }
